9d330e96118b60c4c52cb6c697d37c263e30240c
oshibusu <oshibu.matryo@gmail.com>
2026-01-28 15:29:46 +0900

    sigmoidからtanhに変更

--- CoordAttention.py
+++ CoordAttention.py
@@ -5,7 +5,7 @@ from cvnn.layers import ComplexConv2D, ComplexAvgPooling2D, ComplexBatchNormaliz
 from cvnn.activations import cart_relu
 from keras import backend as K
 import tensorflow as tf
-from SAR_utils import ModSigmoid
+from SAR_utils import ModTanhScaled
 
 
 class ComplexSplit(tf.keras.layers.Layer):
@@ -48,9 +48,9 @@ def CoordAtt_cmplx(x, reduction = 8):
     
     x_w = K.permute_dimensions(x_w, [0, 2, 1, 3])
     a_h = ComplexConv2D(filters=c, kernel_size=(1, 1), strides=(1, 1), padding='valid', activation=None)(x_h)
-    a_h = ModSigmoid()(a_h)
+    a_h = ModTanhScaled()(a_h)
     a_w = ComplexConv2D(filters=c, kernel_size=(1, 1), strides=(1, 1), padding='valid', activation=None)(x_w)
-    a_w = ModSigmoid()(a_w)
+    a_w = ModTanhScaled()(a_w)
     out = x * (a_h * a_w)
     return out
 
